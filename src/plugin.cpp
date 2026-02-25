3cd6a3a65aa4cdd17ae5602e709adfcc2b8be944
gitsgood <christoforoskhouri@gmail.com>
2026-02-25 23:03:55 +0100

    SoulGems Work

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,38 +1,41 @@
-#include "log.h"
+#include "SKSE/SKSE.h"
+#include "RE/Skyrim.h"
 
-
-void OnDataLoaded()
+void OnMessage(SKSE::MessagingInterface::Message* a_message)
 {
-   
+    // Wait until all ESM/ESP/ESL mods have finished loading
+    if (a_message->type == SKSE::MessagingInterface::kDataLoaded) {
+
+        auto* dataHandler = RE::TESDataHandler::GetSingleton();
+        if (!dataHandler) return;
+
+        // Fetch the array of every Soul Gem loaded by the game and mods
+        const auto& soulGems = dataHandler->GetFormArray<RE::TESSoulGem>();
+        size_t patchedCount = 0;
+
+        // Iterate through and set the weight to 0
+        for (auto* gem : soulGems) {
+            if (gem) {
+                // TESSoulGem inherits from TESWeightForm, giving us access to weight
+                gem->weight = 0.0f;
+                patchedCount++;
+            }
+        }
+
+        SKSE::log::info("Successfully set the weight of {} soul gems to 0.", patchedCount);
+    }
 }
 
-void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+SKSEPluginLoad(const SKSE::LoadInterface* skse)
 {
-	switch (a_msg->type) {
-	case SKSE::MessagingInterface::kDataLoaded:
-        
-		break;
-	case SKSE::MessagingInterface::kPostLoad:
-		break;
-	case SKSE::MessagingInterface::kPreLoadGame:
-		break;
-	case SKSE::MessagingInterface::kPostLoadGame:
-        break;
-	case SKSE::MessagingInterface::kNewGame:
-		break;
-	}
-}
-
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+    // Initialize SKSE
     SKSE::Init(skse);
-	SetupLog();
 
-
+    // Register our event listener
     auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
-		return false;
-	}
+    if (messaging) {
+        messaging->RegisterListener(OnMessage);
+    }
 
-	
     return true;
 }
