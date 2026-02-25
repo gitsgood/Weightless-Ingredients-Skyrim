351bc93edfebf2f5e086891fd6a71a7652ab1e5b
gitsgood <christoforoskhouri@gmail.com>
2026-02-25 23:01:15 +0100

    Revert "Fucking works"
    This reverts commit fae134a9c6281c3626aec8f87e6c2df22a972373.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,43 +1,38 @@
-#include "SKSE/SKSE.h"
-#include "RE/Skyrim.h"
+#include "log.h"
 
-// The listener that waits for the game to finish loading mods
-void OnMessage(SKSE::MessagingInterface::Message* a_message)
+
+void OnDataLoaded()
 {
-    // kDataLoaded fires exactly once, right after all ESM/ESP/ESL files have finished loading.
-    if (a_message->type == SKSE::MessagingInterface::kDataLoaded) {
-
-        auto* dataHandler = RE::TESDataHandler::GetSingleton();
-        if (!dataHandler) return;
-
-        // Get the array of all Ingredient Base Forms in the game
-        const auto& ingredients = dataHandler->GetFormArray<RE::IngredientItem>();
-        size_t patchedCount = 0;
-
-        // Iterate through all of them and overwrite the weight to 0.0
-        for (auto* ingredient : ingredients) {
-            if (ingredient) {
-                // IngredientItem inherits from TESWeightForm
-                ingredient->weight = 0.0f;
-                patchedCount++;
-            }
-        }
-
-        SKSE::log::info("Successfully set the weight of {} ingredients to 0.", patchedCount);
-    }
+   
 }
 
-// Standard SKSE NG entry point
-SKSEPluginLoad(const SKSE::LoadInterface* skse)
+void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 {
-    // Initialize SKSE
+	switch (a_msg->type) {
+	case SKSE::MessagingInterface::kDataLoaded:
+        
+		break;
+	case SKSE::MessagingInterface::kPostLoad:
+		break;
+	case SKSE::MessagingInterface::kPreLoadGame:
+		break;
+	case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+	case SKSE::MessagingInterface::kNewGame:
+		break;
+	}
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
+	SetupLog();
 
-    // Register our event listener
+
     auto messaging = SKSE::GetMessagingInterface();
-    if (messaging) {
-        messaging->RegisterListener(OnMessage);
-    }
+	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+		return false;
+	}
 
+	
     return true;
 }
